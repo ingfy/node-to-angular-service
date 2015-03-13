b772c622200c62b08a36a7a7160725dcd6ff09d8
yngsva <yngve.svalestuen@ciber.com>
2015-03-13 16:00:31 +0100

    can work with content directly

--- index.js
+++ index.js
@@ -3,40 +3,51 @@
 var path = require('path'),
     fs = require('fs');
 
-module.exports = function (file, opt, cb) {
-    if (!file) {
+module.exports = function (opt, cb) {
+    if (!opt.file && (!opt.contents && opt.contents !== '')) {
         throw Error('Missing file: node-to-angular-service');
     }
 
     opt = opt || {};
 
-    var fileName;
+    if (opt.file) {
+        var fileName;
 
-    if (typeof file === 'string') {
-        fileName = file;
-    } else if (typeof file.path === 'string') {
-        fileName = path.basename(file.path);
+        if (typeof opt.file === 'string') {
+            fileName = opt.file;
+        } else if (typeof opt.file.path === 'string') {
+            fileName = path.basename(opt.file.path);
+        }
+
+        if (!opt.moduleName) opt.moduleName = fileName;
+        if (!opt.serviecName) opt.serviceName = fileName;
     }
 
-    if (!opt.moduleName) opt.moduleName = fileName;
-    if (!opt.serviecName) opt.serviceName = fileName;
-
     var shell = opt.shellFile || './shell.js';
+
+
 
     fs.readFile(shell, opt.shellEncoding || 'utf8', function (err, shellContents) {
         if (err) cb(err);
 
-        fs.readFile(file, opt.encoding || 'utf8', function (err, contents) {
-            if (err) cb(err);
-
+        function process(contents) {
             var result = shellContents
                 .replace('/* inject: moduleName */', '\'' + opt.moduleName + '\'')
                 .replace('/* inject: serviceName */', '\'' + opt.serviceName + '\'')
                 .replace('/* inject: file */', contents);
 
             cb(null, result);
+        }
 
-        });
+        if (opt.contents) {
+            process(opt.contents);
+        } else {
+            fs.readFile(opt.file, opt.encoding || 'utf8', function (err, contents) {
+                if (err) cb(err);
 
+                process(contents);
+
+            });
+        }
     });
 };
--- test/test.js
+++ test/test.js
@@ -3,12 +3,25 @@ var assert = require('assert');
 var node2angular = require('../index.js');
 
 describe('index.js', function () {
-    it('should produce output', function (cb) {
-        node2angular('./test/testfile.js', {moduleName: 'my.app', serviceName: 'myModule'}, function (err, result) {
+    it('should produce output', function () {
+        node2angular({file: './test/testfile.js', moduleName: 'my.app', serviceName: 'myModule'}, function (err, result) {
             assert.equal(err, null);
             assert.notEqual(result, null);
 
-            cb();
+            done();
+        });
+
+
+    });
+
+    it('should read contents', function () {
+        var fs = require('fs');
+        var contents = fs.readFileSync('./test/testfile.js');
+        node2angular({contents: contents, moduleName: 'my.app', serviceName: 'myModule'}, function (err, result) {
+            assert.equal(err, null);
+            assert.notEqual(result, null);
+
+            done();
         });
     });
 });
